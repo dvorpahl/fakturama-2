3b38e8409451fe84eddc7054bc73c5d446cc99df
Ralf Heydenreich <rheydenr@justmail.de>
2025-08-18 23:01:27 +0200

    preferences repariert (default)

--- Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/Activator.java
+++ Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/Activator.java
@@ -31,6 +31,7 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
 import com.opcoach.e4.preferences.IPreferenceStoreProvider;
+import com.sebulli.fakturama.preferences.DefaultValuesInitializer;
 import com.sebulli.fakturama.preferences.FakturamaPreferenceStoreProvider;
 
 // import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -83,6 +84,8 @@ public class Activator implements BundleActivator, IBundleGroupProvider {
 
         // register preference store provider
         bundleContext.registerService(IPreferenceStoreProvider.class, FakturamaPreferenceStoreProvider.getInstance(), null);
+        new DefaultValuesInitializer().initializeDefaultPreferences(); 
+        
         //        generatePersistenceUnits();
     }
 
