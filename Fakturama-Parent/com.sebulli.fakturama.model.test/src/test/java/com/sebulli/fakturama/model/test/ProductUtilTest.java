bb43418137600caeaf8cf5c0a854e1536c6a0af9
Boarschti <fakturama@rekn.de>
2025-09-07 21:03:24 +0200

    Bugfixes

--- Fakturama-Parent/com.sebulli.fakturama.model.test/src/test/java/com/sebulli/fakturama/model/test/ProductUtilTest.java
+++ Fakturama-Parent/com.sebulli.fakturama.model.test/src/test/java/com/sebulli/fakturama/model/test/ProductUtilTest.java
@@ -1,30 +1,25 @@
-/* 
- * Fakturama - Free Invoicing Software - http://www.fakturama.org
+/* Fakturama - Free Invoicing Software - http://www.fakturama.org
  * 
  * Copyright (C) 2019 www.fakturama.org
  * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     The Fakturama Team - initial API and implementation
- */
- 
+ * Contributors: The Fakturama Team - initial API and implementation */
+
 package com.sebulli.fakturama.model.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.e4.core.contexts.ContextInjectionFactory;
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.sebulli.fakturama.misc.Constants;
@@ -36,55 +31,52 @@ import com.sebulli.fakturama.util.ProductUtil;
  */
 @ExtendWith(MockitoExtension.class)
 class ProductUtilTest {
-    
-	Double quantityOfOne = Double.valueOf(1.0);
-    
+
+    Double quantityOfOne = Double.valueOf(1.0);
+
     @Mock
     protected IEclipsePreferences preferences;
-    protected IEclipseContext context;
-    
-	private ProductUtil productUtil;
 
-    @BeforeAll
-	public void setUp() throws Exception {
-		MockitoAnnotations.openMocks(this);
-        this.productUtil = ContextInjectionFactory.make(ProductUtil.class, context);
-	}
+    @InjectMocks
+    private ProductUtil productUtil;
 
-	@Test
+    @Test
+    @DisplayName("Test if price without given scale delivers correct result")
     void testGetPriceByQuantity_withoutScales_success() {
         Mockito.when(preferences.getInt(Mockito.eq(Constants.PREFERENCES_PRODUCT_SCALED_PRICES), Mockito.anyInt())).thenReturn(1);
         productUtil.setEclipsePrefs(preferences);
         Product product = new Product();
-		product.setPrice1(Double.valueOf(1.0));
+        product.setPrice1(Double.valueOf(1.0));
         Double result = productUtil.getPriceByQuantity(product, quantityOfOne);
         assertEquals(quantityOfOne, result);
-	}
-	
-	@Test
+    }
+
+    @Test
+    @DisplayName("Test if price without given scale delivers right scaled price")
     void testGetPriceByQuantity_withoutScales_scaledPrice() {
 
-        Mockito.when(preferences.getInt(Constants.PREFERENCES_PRODUCT_SCALED_PRICES, Mockito.anyInt())).thenReturn(1);
+        Mockito.when(preferences.getInt(Mockito.eq(Constants.PREFERENCES_PRODUCT_SCALED_PRICES), Mockito.anyInt())).thenReturn(1);
         productUtil.setEclipsePrefs(preferences);
-		Product product = new Product();
-		product.setPrice1(Double.valueOf(10.0));
-		product.setPrice2(Double.valueOf(8.0));
-		product.setBlock1(Integer.valueOf(1));
-		product.setBlock2(Integer.valueOf(10));
-		Double result = productUtil.getPriceByQuantity(product, quantityOfOne);
-		assertEquals(Double.valueOf(10.0), result);
-	}
-	
-	@Test
+        Product product = new Product();
+        product.setPrice1(Double.valueOf(10.0));
+        product.setPrice2(Double.valueOf(8.0));
+        product.setBlock1(Integer.valueOf(1));
+        product.setBlock2(Integer.valueOf(10));
+        Double result = productUtil.getPriceByQuantity(product, quantityOfOne);
+        assertEquals(Double.valueOf(10.0), result);
+    }
+
+    @Test
+    @DisplayName("Test if price with given scale delivers scaled price")
     void testGetPriceByQuantity_withScales_scaledPrice() {
-		Mockito.when(preferences.getInt(Mockito.eq(Constants.PREFERENCES_PRODUCT_SCALED_PRICES), Mockito.anyInt())).thenReturn(Integer.valueOf(2));
+        Mockito.when(preferences.getInt(Mockito.eq(Constants.PREFERENCES_PRODUCT_SCALED_PRICES), Mockito.anyInt())).thenReturn(Integer.valueOf(2));
         productUtil.setEclipsePrefs(preferences);
-		Product product = new Product();
-		product.setPrice1(Double.valueOf(10.0));
-		product.setPrice2(Double.valueOf(8.0));
-		product.setBlock1(Integer.valueOf(1));
-		product.setBlock2(Integer.valueOf(10));
-		Double result = productUtil.getPriceByQuantity(product, Double.valueOf(10.0));
-		assertEquals(Double.valueOf(8.0), result);
-	}
+        Product product = new Product();
+        product.setPrice1(Double.valueOf(10.0));
+        product.setPrice2(Double.valueOf(8.0));
+        product.setBlock1(Integer.valueOf(1));
+        product.setBlock2(Integer.valueOf(10));
+        Double result = productUtil.getPriceByQuantity(product, Double.valueOf(10.0));
+        assertEquals(Double.valueOf(8.0), result);
+    }
 }
